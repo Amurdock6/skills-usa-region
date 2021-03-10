2ee0e9151c3de9ad8da008536454d9de662a31b2
Amurdock6 <77793000+Amurdock6@users.noreply.github.com>
2021-03-10 09:22:25 -0700

    rev 5

--- js/main.js
+++ js/main.js
@@ -16,23 +16,23 @@ function myFunction() {
   }
 }
 
-var sliderMain = function() {
+// var sliderMain = function() {
 		
-    $('#slide-show .flexslider').flexslider({
-      animation: "fade",
-      slideshowSpeed: 5000,
-      directionNav: true,
-      start: function(){
-          setTimeout(function(){
-              $('.slider-text').removeClass('animated fadeInUp');
-              $('.flex-active-slide').find('.slider-text').addClass('animated fadeInUp');
-          }, 500);
-      },
-      before: function(){
-          setTimeout(function(){
-              $('.slider-text').removeClass('animated fadeInUp');
-              $('.flex-active-slide').find('.slider-text').addClass('animated fadeInUp');
-          }, 500);
-      }
+//     $('#slide-show .flexslider').flexslider({
+//       animation: "fade",
+//       slideshowSpeed: 5000,
+//       directionNav: true,
+//       start: function(){
+//           setTimeout(function(){
+//               $('.slider-text').removeClass('animated fadeInUp');
+//               $('.flex-active-slide').find('.slider-text').addClass('animated fadeInUp');
+//           }, 500);
+//       },
+//       before: function(){
+//           setTimeout(function(){
+//               $('.slider-text').removeClass('animated fadeInUp');
+//               $('.flex-active-slide').find('.slider-text').addClass('animated fadeInUp');
+//           }, 500);
+//       }
 
-    });
+//     });
